1a1aef644c6a5221288a0aec0671c36b042da0ff
Dmitrii <dsavelev1703@yandex.ru>
2024-12-21 19:34:53 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://stihi.ru/pics/2023/03/07/6669.jpg" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <img
+        src="https://i.pinimg.com/originals/49/e3/f1/49e3f1b4e3b12b401f7f1066b4233015.jpg"
+        alt="NY"
+      />
+      <div>ava+ description</div>
+      <div>
+        My posts <div>new post</div>
+      </div>
+      <div>Post 1</div>
+      <div>Post 2</div>
+    </div>
+  );
+};
+
+export default Profile;
